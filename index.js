8b85db88be97fc6ef12a824444da2f66d9ea6d4f
jdtzmn <jdtzmn@gmail.com>
2018-12-06 21:43:58 -0500

    Cache failed requests

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const axios = require('axios')
 
 const fetchTableData = require('./fetchTableData')
-const { createFormSubmitter } = require('./formSubmission')
 const question = require('./substitutionHelpers')
 const { formId } = require('./form.config')
 const handleEdits = require('./handleEdits')
+const constructPromises = require('./constructPromises')
 
 const instance = axios.create({
   baseURL: `https://docs.google.com/forms/d/e/${formId}`
 })
-const sendFormSubmission = createFormSubmitter(instance)
 
 const main = async () => {
   const entries = await fetchTableData(instance)
 
   // Make sure the form looks correct
   console.log(entries)
+  console.log(`There are ${Object.keys(entries).length} entries`)
   const response = await question('Does this look right? Should the form be submitted? If it needs edits, type `Edit`.', ['Yes', 'No', 'Edit'])
 
   switch (response) {
@@ -25,12 +25,15 @@ const main = async () => {
     case 'Edit':
       await handleEdits()
       return main() // run the program again with the updated values
+    case 'Yes':
   }
 
   console.log('Submitting the form...')
-  const promises = entries.map(sendFormSubmission)
-  await Promise.all(promises)
+  const formPromises = constructPromises(entries, instance)
+  const responses = await Promise.all(formPromises.promises)
+  formPromises.writeFile() // save the responses
   console.log('Submitted!')
+  console.log(responses.filter((x) => x))
 }
 
 main()
