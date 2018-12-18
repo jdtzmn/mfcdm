00f157f797a17c9bf7f23d1c375a6a665a2ee0c3
jdtzmn <jdtzmn@gmail.com>
2018-12-17 21:55:57 -0500

    Added a prettier CLI

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const { term } = require('./lib/terminal')
+const { term, terminate } = require('./lib/terminal')
 
 // commands
 const submitCommand = require('./lib/commands/submit')
+const verifyCommand = require('./lib/commands/verify')
+const analyzeCommand = require('./lib/commands/analyze')
+const convertCommand = require('./lib/commands/convert')
 
 term.clear()
 
 term('What would you like to do?\n')
-const { promise } = term.singleColumnMenu(['Submit Form', 'Convert Data', 'Analyze Data'])
-promise.then((command) => {
-  switch (command.selectedText) {
+const { promise } = term.singleColumnMenu([
+  'Convert Data',
+  'Verify Data',
+  'Analyze Data',
+  'Submit Form',
+  'Quit'
+])
+promise.then(({ selectedText }) => {
+  switch (selectedText) {
     case 'Submit Form':
-      submitCommand()
+      submitCommand(term)
       break
+    case 'Verify Data':
+      verifyCommand(term)
+      break
+    case 'Analyze Data':
+      analyzeCommand(term)
+      break
+    case 'Convert Data':
+      convertCommand(term)
+      break
+    default:
+      terminate()
   }
 })
